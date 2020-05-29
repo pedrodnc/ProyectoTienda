8018458a3375ab1e5f4ebc4e0a8f34ca54fe412e
pedrodnc <56719748+pedrodnc@users.noreply.github.com>
2020-05-29 16:51:07 +0200

    ConstructorUsuario

--- src/Clases/Usuario.java
+++ src/Clases/Usuario.java
@@ -1,5 +1,24 @@
 package Clases;
 
 public class Usuario {
+	
+	private String nombreAcceso;// Nombre de cada objeto persona    
+    private String pass;// Contraseña de cada objeto persona
+    private String numeroDocumentoidentidad;// DNI de cada objeto persona
+    private int edad; //Edad de cada objeto eprsona
+    
+    
+	public Usuario(String nombreAcceso, String pass, String numeroDocumentoidentidad, int edad) {
+		super();
+		this.nombreAcceso = nombreAcceso;
+		this.pass = pass;
+		this.numeroDocumentoidentidad = numeroDocumentoidentidad;
+		this.edad = edad;
+	}
+    
+   
+
+    
+    
 
 }
