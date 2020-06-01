4e3de85925fd3396949a3b67e3350753271aaa59
pedrodnc <56719748+pedrodnc@users.noreply.github.com>
2020-06-01 18:45:33 +0200

    1/06
    Creación clase cliente, modificación clase Usuario

--- src/Clases/Usuario.java
+++ src/Clases/Usuario.java
@@ -4,18 +4,66 @@ public class Usuario {
 	
 	private String nombreAcceso;// Nombre de cada objeto persona    
     private String pass;// Contraseña de cada objeto persona
-    private String numeroDocumentoidentidad;// DNI de cada objeto persona
-    private int edad; //Edad de cada objeto eprsona
+    private String numeroDNI;// DNI de cada objeto persona
+    private boolean estado; //Estado del usuario. Conctado/desconectado
     
     
-	public Usuario(String nombreAcceso, String pass, String numeroDocumentoidentidad, int edad) {
+    
+	public Usuario(String nombreAcceso, String pass, String numeroDNI) {
 		super();
 		this.nombreAcceso = nombreAcceso;
 		this.pass = pass;
-		this.numeroDocumentoidentidad = numeroDocumentoidentidad;
-		this.edad = edad;
+		this.numeroDNI = numeroDNI;
+		
 	}
-    
+	
+	
+	public String getNombreAcceso() {
+		return nombreAcceso;
+	}
+
+
+	public void setNombreAcceso(String nombreAcceso) {
+		this.nombreAcceso = nombreAcceso;
+	}
+
+
+	public String getPass() {
+		return pass;
+	}
+
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+	public String getNumeroDNI() {
+		return numeroDNI;
+	}
+
+
+	public void setNumeroDNI(String numeroDNI) {
+		this.numeroDNI = numeroDNI;
+	}
+
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+
+	public void leerBD() {
+		
+	}
+    public void grabarBD() {
+    	
+    }
    
 
     
